ee2b66fa2095533fa76a0a533535bae9c74c59c6
rabeya121 <122045320+rabeya121@users.noreply.github.com>
2025-02-01 05:23:55 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,11 +2,17 @@ const accountId = 14553
 let accountEmail = 'Rabeya@gmail.com'
 var accountPassword = '123456'
 accountCity = 'Narail'
+let accountSate;
 
-// accountId = 2
+// accountId = 2 //not allowed
+
+
 accountEmail = 'bicrom2@gmail.com'
 accountPassword = '123123'
 accountCity = 'Naogoun'
 console.log(accountId);
-console.table ([accountId,accountEmail,accountPassword,accountCity]);
+/* Prefer not to use Var
+because of issue in block scope and functional scope
+*/
+console.table ([accountId,accountEmail,accountPassword,accountCity , accountSate]);
 
