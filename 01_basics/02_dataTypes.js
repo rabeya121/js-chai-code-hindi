b44592eb4c885a44d0df9459a41eeb1a5ac5bd61
rabeya121 <122045320+rabeya121@users.noreply.github.com>
2025-02-01 06:21:57 +0000

    conversions

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer vertions
+
+//alert(3+3) //we are using node js, not browser;
+
+console.log(3+ 
+    3) // code readability should be high
+
+console.log('Rabeya') 
+
+let name = "Rabeya"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone valu
+//undifane =>
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof 19);
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
+
+
---
+++ 01_basics/03_conversionOparetions.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valuInNumber = Number (score);
+console.log(typeof  valuInNumber );
+
+// "33"=> 33
+//"33abc" => Nan
+//true => 1, false =>0
+//null => 0
+// undefined =>Nan
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log ( booleanIsLoggedIn );
+
+//1 => true,0 => false
+//" " => false
+//"Rabeya" => true
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
