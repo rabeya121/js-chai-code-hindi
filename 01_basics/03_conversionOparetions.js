c35d56eac1f9e032fa83530267f92c3fb55e08c6
rabeya121 <122045320+rabeya121@users.noreply.github.com>
2025-02-02 05:37:03 +0000

    comparistion

--- 01_basics/03_conversionOparetions.js
+++ 01_basics/03_conversionOparetions.js
@@ -1,11 +1,11 @@
 let score = "33"
 
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valuInNumber = Number (score);
-console.log(typeof  valuInNumber );
+//console.log(typeof  valuInNumber );
 
 // "33"=> 33
 //"33abc" => Nan
@@ -16,7 +16,7 @@ console.log(typeof  valuInNumber );
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log ( booleanIsLoggedIn );
+//console.log ( booleanIsLoggedIn );
 
 //1 => true,0 => false
 //" " => false
@@ -24,8 +24,32 @@ console.log ( booleanIsLoggedIn );
 
 let someNumber = 33
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//************************Oparetions***************************
+
+
+let valu = 3
+let negValu = -valu 
+//console.log(negValu);
+
+let srt1= "hello"
+let srt2 = " Rabeya"
+let srt3 = srt1 + srt2
+// console.log(srt3);
+
+// console.log ("1"+2);
+// console.log("1"+2+4);
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
 
 
 
